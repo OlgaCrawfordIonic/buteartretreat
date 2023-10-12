76fa0ed05c1677eabcc334fb0479f012da7d7bb2
Olga-Crawford <olgacrawford100@yahoo.com>
2023-10-12 15:59:43 +0100

    second commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.buteartretreat.starter',
   appName: 'buteartretreat',
   webDir: 'www',
   server: {
